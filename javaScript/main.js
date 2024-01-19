1836473bd1a6e1e63048d541d201ed5ba5676281
Matheus Mizuno <matheuseikomizuno@gmail.com>
2024-01-18 23:20:27 -0300

    Conversor de Dolar

---
+++ javaScript/main.js
@@ -0,0 +1,38 @@
+const btn = document.getElementById('send').addEventListener('click', function (e){
+    e.preventDefault();
+    fetch('https://cdn.moeda.info/api/latest.json').then(r=>{
+        return r.json()
+    }).then(corpo=>{
+
+        const dl = document.getElementById('valor_dolar')
+        const dollar_inp = valor_dolar.value
+        const getMoedas = document.getElementById('more')
+        const moedas = more.value
+        const  res = document.getElementById('result')
+
+        switch(more.value){
+            case 'Real':
+                const real_convert = valor_dolar.value*corpo.rates.BRL
+                result.textContent = real_convert.toFixed(2)
+                break;
+                case 'Iene'://jpy
+                const iene_converte = valor_dolar.value*corpo.rates.JPY
+                result.textContent = iene_converte.toFixed(2)
+                  
+                    break;
+                    case 'Euro':
+                        const euro_convert = valor_dolar.value*corpo.rates.EUR
+                        result.textContent = euro_convert.toFixed(2)
+                       
+                        break;
+        }
+
+        
+})
+})
+
+const cl = document.getElementById("clear").addEventListener("click", function (){
+        result.textContent = ""
+        valor_dolar.value = ''
+        more.value = ''
+    }) 

5a734958d77f6e157c36c20fabdbfc90866dab56
Matheus Mizuno <matheuseikomizuno@gmail.com>
2024-01-21 15:11:02 -0300

    Alterações no design e adição de moeda

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,50 +1,22 @@
-const btn = document.getElementById('send').addEventListener('click', function (e){
-    e.preventDefault();
-    fetch('https://cdn.moeda.info/api/latest.json').then(r=>{
-        return r.json()
-    }).then(corpo=>{
-
-        const dl = document.getElementById('valor_dolar')
-        const dollar_inp = valor_dolar.value
-        const getMoedas = document.getElementById('more')
-        const moedas = more.value
-        const  res = document.getElementById('result')
-        const troca = document.getElementById('switch')
+const dl = document.getElementById('valor_dolar')
+const dollar_inp = valor_dolar.value
+const  res = document.getElementById('result')
 
 
-        switch(more.value){
-            case 'Real':
-                let real_convert = valor_dolar.value*corpo.rates.BRL
-                result.textContent = `${real_convert.toFixed(2)} Reais`
-                troca.onclick = function(){
-                    real_convert = valor_dolar.value/ corpo.rates.BRL
-                    result.textContent = `${real_convert.toFixed(2)} Dólares`
-                }
+fetch('https://cdn.moeda.info/api/latest.json').then(r=>{
+    return r.json()
+}).then(corpo=>{ 
+    const moedas = document.querySelector('[data-js="allmoedas"]')
+    const options = Object.keys(corpo.rates).map(currency => `<option>${currency}</option>`)
+    moedas.innerHTML = options
 
-                break;
-                case 'Iene'://jpy
-                let iene_converte = valor_dolar.value*corpo.rates.JPY
-                result.textContent = `${iene_converte.toFixed(2)} Ienes`
-                troca.onclick = function(){
-                    iene_converte = (corpo.rates.JPY/corpo.rates.JPY)*valor_dolar.value
-                    result.textContent = `${iene_converte} Dólares`
-                }
-
-                  
-                    break;
-                    case 'Euro':
-                        let euro_convert = valor_dolar.value*corpo.rates.EUR
-                        result.textContent = `${euro_convert.toFixed(2)} Euros `
-                        troca.onclick = function(){
-                            euro_convert = valor_dolar.value/corpo.rates.EUR
-                            result.textContent =`${euro_convert.toFixed(2)} Dólares` 
-                            }
-                       
-                        break;
-        }
-
-        
-})
+    const btn = document.getElementById('send').addEventListener('click', function (e){
+        e.preventDefault();
+        const allmoedas = document.getElementById('more')
+        const Tmoedas = more.value
+        const conversao = valor_dolar.value * corpo.rates[Tmoedas]
+        result.textContent = `${conversao.toFixed(2)} ${Tmoedas}`
+    }) 
 })
 
 const cl = document.getElementById("clear").addEventListener("click", function (){
@@ -52,4 +24,5 @@ const cl = document.getElementById("clear").addEventListener("click", function (
         valor_dolar.value = ''
         more.value = ''
     }) 
-    AOS.init();
+   
+  AOS.init();
